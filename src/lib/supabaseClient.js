0dd12ecad7f3ce42a2277ff42b395ac43a3f4083
Aidai <aidai@example.com>
2026-06-03 00:01:03 +0600

    env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// Проверка на случай, если переменные не загрузились
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase URL or key is not defined in .env.local')
+  console.error('Ошибка: Переменные Supabase не найдены в .env.local')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
